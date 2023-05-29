56e1cc085d2116dd29dbdfdfcc361c7a097f60bf
juanarancibia <arancibia.juan22@gmail.com>
2023-05-29 11:37:39 -0300

    Introducción y Configuracion Inicial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { RococoContractsTestnet } from "useink/chains";
+import dynamic from "next/dynamic";
+
+const UseInkProvider = dynamic(
+  import("useink").then((c) => c.UseInkProvider),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UseInkProvider
+      config={{
+        dappName: "useink dApp",
+        chains: [RococoContractsTestnet],
+      }}
+    >
+      <Component {...pageProps} />
+    </UseInkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
