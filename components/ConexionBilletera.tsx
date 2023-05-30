d81c8a76de109b1f10c760cfd33ab37ceb715133
juanarancibia <arancibia.juan22@gmail.com>
2023-05-30 20:26:31 -0300

    feat: agregar conexion billetera

---
+++ components/ConexionBilletera.tsx
@@ -0,0 +1,59 @@
+import DownloadIcon from "@mui/icons-material/Download";
+import LoginIcon from "@mui/icons-material/Login";
+import { Button, Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Image from "next/image";
+import { FC } from "react";
+import { useInstalledWallets, useUninstalledWallets, useWallet } from "useink";
+
+export const ConexionBilletera: FC<{}> = () => {
+  const { connect, disconnect } = useWallet();
+  const installedWallets = useInstalledWallets();
+  const uninstalledWallets = useUninstalledWallets();
+
+  return (
+    <Stack alignItems="start" justifyContent="center">
+      <Typography variant="h5" paddingY={3} fontWeight="bold">
+        BILLETERAS INSTALADAS
+      </Typography>
+
+      {installedWallets.map((w) => (
+        <Button
+          key={w.title}
+          fullWidth
+          onClick={() => connect(w.extensionName)}
+        >
+          <Image src={w.logo.src} alt={w.logo.alt} width={50} height={50} />
+
+          <Typography paddingX={3} textAlign="left" width={"100%"}>
+            {w.title}
+          </Typography>
+
+          <LoginIcon />
+        </Button>
+      ))}
+
+      <Typography variant="h5" paddingY={3} fontWeight="bold">
+        BILLETERAS NO INSTALADAS
+      </Typography>
+
+      {uninstalledWallets.map((w) => (
+        <Button
+          key={w.title}
+          fullWidth
+          onClick={() => window.open(w.installUrl, "_blank")}
+        >
+          <Image src={w.logo.src} alt={w.logo.alt} width={50} height={50} />
+
+          <Typography paddingX={3} textAlign="left" width={"100%"}>
+            {w.title}
+          </Typography>
+
+          <DownloadIcon />
+        </Button>
+      ))}
+    </Stack>
+  );
+};
+
+export default ConexionBilletera;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import ConexionBilletera from "../components/ConexionBilletera";
+import Stack from "@mui/material/Stack";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +11,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <Stack alignItems="center" justifyContent="center">
+          <ConexionBilletera />
+        </Stack>
+      </main>
     </div>
   );
 };
