3344d5426444e40fa31048c2a84bbabb6042c03f
juanarancibia <arancibia.juan22@gmail.com>
2023-06-08 11:17:41 -0300

    feat: set material theme to dark; finalize account selector

--- components/InformacionCuenta.tsx
+++ components/InformacionCuenta.tsx
@@ -1,16 +1,18 @@
 import { Stack, Typography } from "@mui/material";
 import { FC } from "react";
 import { useWallet } from "useink";
+import SelectorCuenta from "./SelectorCuenta";
 
 export const InformacionCuenta: FC<{}> = () => {
   const { account } = useWallet();
 
   return (
     <Stack height="fit-content">
-      <Typography variant="h5" fontWeight="bold" marginBottom={2}>
-        CUENTA CONECTADA
-      </Typography>
+      <SelectorCuenta />
 
+      <Typography variant="h5" fontWeight="bold" marginTop={4} marginBottom={2}>
+        Cuenta seleccionada
+      </Typography>
       <Typography color="grey">Nombre: {account?.name}</Typography>
       <Typography color="grey">
         Dirección: {account?.address.substring(0, 15)}...
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,31 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RococoContractsTestnet } from "useink/chains";
 import dynamic from "next/dynamic";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 const UseInkProvider = dynamic(
   import("useink").then((c) => c.UseInkProvider),
   { ssr: false }
 );
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UseInkProvider
-      config={{
-        dappName: "useink dApp",
-        chains: [RococoContractsTestnet],
-      }}
-    >
-      <Component {...pageProps} />
-    </UseInkProvider>
+    <ThemeProvider theme={darkTheme}>
+      <UseInkProvider
+        config={{
+          dappName: "useink dApp",
+          chains: [RococoContractsTestnet],
+        }}
+      >
+        <Component {...pageProps} />
+      </UseInkProvider>
+    </ThemeProvider>
   );
 }
 
