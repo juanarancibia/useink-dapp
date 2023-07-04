80625aea5be4452408fa79da79fae340072d054b
juanarancibia <arancibia.juan22@gmail.com>
2023-07-04 10:11:36 -0300

    Revert "feat: delete features for other branch"
    This reverts commit 75e21c6ba91b4d1161d2130706399fca4538c50f.

--- components/ContractInteraction.tsx
+++ components/ContractInteraction.tsx
@@ -1,6 +1,7 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
+import Decimal from "decimal.js";
 import { FC, useEffect } from "react";
-import { useCall, useContract, useWallet } from "useink";
+import { useCall, useContract, useTx, useWallet } from "useink";
 import { pickDecoded } from "useink/utils";
 import { BANK_CONTRACT_ADDRESS_ROC } from "../contracts/addresses";
 import bankContractMetadata from "../contracts/bank.json";
@@ -18,6 +19,9 @@ const ContractInteraction: FC<{}> = () => {
     "getBalanceByAccount"
   );
 
+  const deposit = useTx(bankContract, "deposit");
+  const withdraw = useTx(bankContract, "withdraw");
+
   useEffect(() => {
     getBalanceByAccount.send();
   }, [account, getBalanceByAccount]);
@@ -29,12 +33,50 @@ const ContractInteraction: FC<{}> = () => {
       </Typography>
 
       <Typography paddingBottom={1} textAlign="center">
-        Balance de la cuenta:{" "}
-        {pickDecoded(getBalanceByAccount.result)?.Ok ??
-          pickDecoded(getBalanceByAccount.result)?.Err}
+        {formatAccountDepositBalance(
+          pickDecoded(getBalanceByAccount.result)?.Ok
+        ) ?? pickDecoded(getBalanceByAccount.result)?.Err}
       </Typography>
+
+      <Button
+        onClick={() =>
+          deposit.signAndSend([], {
+            value: formatNumber(0.1, 12),
+          })
+        }
+      >
+        Depositar 0.1 ROC
+      </Button>
+      <Button
+        disabled={!pickDecoded(getBalanceByAccount.result)?.Ok}
+        onClick={() => withdraw.signAndSend([formatNumber(0.1, 12)])}
+      >
+        Retirar 0.1 ROC
+      </Button>
     </Stack>
   );
+};
+
+const formatAccountDepositBalance = (
+  accountBalance: string | undefined
+): string | undefined => {
+  return accountBalance
+    ? `Depósito de cuenta: ${formatNumber(
+        +accountBalance.replaceAll(",", ""),
+        12,
+        false
+      )} ROC`
+    : undefined;
+};
+
+const formatNumber = (
+  value: number,
+  decimals: number,
+  toInteger: boolean = true
+): number => {
+  return new Decimal(value)
+    .mul(new Decimal(10).pow((toInteger ? 1 : -1) * decimals))
+    .toNumber();
 };
 
 export default ContractInteraction;
